aaff13573f7cefb6feaa21a7b465934d5c46a2f9
Elliott-Roach <elliott.roach@mths.ca>
2024-10-16 11:04:49 -0400

    egsakt same as demo

--- main.ts
+++ main.ts
@@ -5,21 +5,19 @@
  * This program takes distance
 */
 
-let distanceInCentimeters:number = 0
+let distanceToObject:number = 0
 
 //clearing
-basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 //finding distance and displaying it
-    input.onButtonPressed(Button.A, function () {
-        basic.clearScreen()
-        distanceInCentimeters = sonar.ping(
-            DigitalPin.P1, 
-            DigitalPin.P2, 
-            PingUnit.Centimeters
-        )
-        basic.showNumber(distanceInCentimeters)
-        pause(5000)
-        basic.showIcon(IconNames.Happy)
-    })
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    distanceToObject = sonar.ping(
+        DigitalPin.P1, 
+        DigitalPin.P2, 
+        PingUnit.Centimeters
+    )
+    basic.showNumber(distanceToObject)
+    basic.showIcon(IconNames.Happy)
+})
