aa48a82ba7f646ec8ca5bd439b316766cce19b2c
Elliott-Roach <elliott.roach@mths.ca>
2024-10-16 10:45:36 -0400

    takes distance

--- main.ts
+++ main.ts
@@ -2,7 +2,21 @@
  *
  * Created by: Elliott Roach
  * Created on: oct 2024
- * This program says hello
+ * This program takes distance
 */
 
-basic.showString('Hello, World!')
+let distanceInCentimeters: number = 0
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+    input.onButtonPressed(Button.A, function () {
+        distanceInCentimeters = sonar.ping(
+            DigitalPin.P1, 
+            DigitalPin.P2, 
+            PingUnit.Centimeters
+            )
+        
+        basic.showNumber(distanceInCentimeters)
+        basic.showIcon(IconNames.Happy)
+    })
