a302518c734ce5096dd721120e3dd1907c1d7a2e
Elliott-Roach <elliott.roach@mths.ca>
2024-10-16 10:56:09 -0400

    shows number for 5 seconds

--- main.ts
+++ main.ts
@@ -5,18 +5,21 @@
  * This program takes distance
 */
 
-let distanceInCentimeters: number = 0
+let distanceInCentimeters:number = 0
 
+//clearing
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
+//finding distance and displaying it
     input.onButtonPressed(Button.A, function () {
+        basic.clearScreen()
         distanceInCentimeters = sonar.ping(
             DigitalPin.P1, 
             DigitalPin.P2, 
             PingUnit.Centimeters
-            )
-        
+        )
         basic.showNumber(distanceInCentimeters)
+        pause(5000)
         basic.showIcon(IconNames.Happy)
     })
